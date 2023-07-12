5f7d2118431d6361ff029b4df82a8a5b512c6e6c
Renata Hassum <renatahassum@gmail.com>
2023-07-12 12:06:12 -0300

    Folder organization

--- backend/src/main/java/com/devsuperior/workshopmongo/models/entities/Post.java
+++ backend/src/main/java/com/devsuperior/workshopmongo/models/entities/Post.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import com.devsuperior.workshopmongo.models.embedded.Author;
+import com.devsuperior.workshopmongo.models.embedded.Comment;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
@@ -128,83 +130,3 @@ public class Post {
     }
 }
 
-class Author {
-
-    private String id;
-    private String name;
-
-    public Author() {
-    }
-
-    public Author(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Author(User entity) {
-        id = entity.getId();
-        name = entity.getName();
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-}
-
-class Comment {
-
-    private String text;
-    private Instant date;
-    private Author author;
-
-    public Comment() {
-    }
-
-    public Comment(String text, Instant date, Author author) {
-        this.text = text;
-        this.date = date;
-        this.author = author;
-    }
-
-    public Comment(String text, Instant date, String authorId, String authorName) {
-        this.text = text;
-        this.date = date;
-        this.author = new Author(authorId, authorName);
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public Instant getDate() {
-        return date;
-    }
-
-    public void setDate(Instant date) {
-        this.date = date;
-    }
-
-    public Author getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(Author author) {
-        this.author = author;
-    }
-}
--- backend/src/main/java/com/devsuperior/workshopmongo/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/workshopmongo/repositories/UserRepository.java
@@ -3,7 +3,6 @@ package com.devsuperior.workshopmongo.repositories;
 import com.devsuperior.workshopmongo.models.entities.User;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveMongoRepository<User, String>{
